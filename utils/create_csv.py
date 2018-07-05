b347c087e05e63b2134b88149b6952a35581dcc9
ahmetozlu <ahmetozlu93@gmail.com>
2018-07-05 20:37:52 +0300

    indentation refactoring

--- utils/create_csv.py
+++ utils/create_csv.py
@@ -33,14 +33,14 @@ def CreateCsv(csvLine):
 
     label = 0
     with open("face_dataset.csv", "w") as file:
-	    writer = csv.writer(file)
-	    for dirname, dirnames, filenames in os.walk(BASE_PATH):
-		for subdirname in dirnames:
-		    subject_path = os.path.join(dirname, subdirname)
-		    for filename in os.listdir(subject_path):
-		        abs_path = "%s/%s" % (subject_path, filename)
-		        csv_line = "%s%s%d" % (abs_path, SEPARATOR, label)
-			print csv_line			    		
-	    		writer.writerows([csv_line.split(',')])
-		    label = label + 1
+            writer = csv.writer(file)
+            for dirname, dirnames, filenames in os.walk(BASE_PATH):
+                for subdirname in dirnames:
+                    subject_path = os.path.join(dirname, subdirname)
+                    for filename in os.listdir(subject_path):
+                        abs_path = "%s/%s" % (subject_path, filename)
+                        csv_line = "%s%s%d" % (abs_path, SEPARATOR, label)
+                        print csv_line                                  
+                        writer.writerows([csv_line.split(',')])
+                    label = label + 1
 
--- vehicle_detection_main.py
+++ vehicle_detection_main.py
@@ -28,9 +28,9 @@ from utils import visualization_utils as vis_util
 
 #initialize .csv
 with open('traffic_measurement.csv', 'w') as f:
-	writer = csv.writer(f)	
-	csv_line = "Vehicle Type/Size, Vehicle Color, Vehicle Movement Direction, Vehicle Speed (km/h)"			
-	writer.writerows([csv_line.split(',')])
+        writer = csv.writer(f)  
+        csv_line = "Vehicle Type/Size, Vehicle Color, Vehicle Movement Direction, Vehicle Speed (km/h)"                 
+        writer.writerows([csv_line.split(',')])
 
 if tf.__version__ < '1.4.0':
   raise ImportError('Please upgrade your tensorflow installation to v1.4.* or later!')
@@ -89,85 +89,86 @@ def load_image_into_numpy_array(image):
 
 # Detection
 def object_detection_function():
-	total_passed_vehicle = 0
-	speed = "waiting..."
-	direction = "waiting..."
-	size = "waiting..."
-	color = "waiting..."
-	with detection_graph.as_default():
-	  with tf.Session(graph=detection_graph) as sess:
-	    # Definite input and output Tensors for detection_graph
-	    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+        total_passed_vehicle = 0
+        speed = "waiting..."
+        direction = "waiting..."
+        size = "waiting..."
+        color = "waiting..."
+        with detection_graph.as_default():
+          with tf.Session(graph=detection_graph) as sess:
+            # Definite input and output Tensors for detection_graph
+            image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
 
-	    # Each box represents a part of the image where a particular object was detected.
-	    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+            # Each box represents a part of the image where a particular object was detected.
+            detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
 
-	    # Each score represent how level of confidence for each of the objects.
-	    # Score is shown on the result image, together with the class label.
-	    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
-	    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
-	    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+            # Each score represent how level of confidence for each of the objects.
+            # Score is shown on the result image, together with the class label.
+            detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+            detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+            num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
-	    # for all the frames that are extracted from input video
-	    while(cap.isOpened()):
-		ret, frame = cap.read()
+            # for all the frames that are extracted from input video
+            while(cap.isOpened()):
+                ret, frame = cap.read()
 
-		if not  ret:
-			print("end of the video file...")
-			break
-		
-	    	input_frame = frame
+                if not  ret:
+                        print("end of the video file...")
+                        break
+                
+                input_frame = frame
 
-		# Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-		image_np_expanded = np.expand_dims(input_frame, axis=0)
+                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+                image_np_expanded = np.expand_dims(input_frame, axis=0)
 
-		# Actual detection.
-		(boxes, scores, classes, num) = sess.run(
-		    [detection_boxes, detection_scores, detection_classes, num_detections],
-		    feed_dict={image_tensor: image_np_expanded})
+                # Actual detection.
+                (boxes, scores, classes, num) = sess.run(
+                    [detection_boxes, detection_scores, detection_classes, num_detections],
+                    feed_dict={image_tensor: image_np_expanded})
 
-		# Visualization of the results of a detection.
-		counter, csv_line = vis_util.visualize_boxes_and_labels_on_image_array(cap.get(1),
-		    input_frame,
-		    np.squeeze(boxes),
-		    np.squeeze(classes).astype(np.int32),
-		    np.squeeze(scores),
-		    category_index,
-		    use_normalized_coordinates=True,
-		    line_thickness=4)
-		
-		total_passed_vehicle = total_passed_vehicle + counter		
+                # Visualization of the results of a detection.
+                counter, csv_line = vis_util.visualize_boxes_and_labels_on_image_array(cap.get(1),
+                    input_frame,
+                    np.squeeze(boxes),
+                    np.squeeze(classes).astype(np.int32),
+                    np.squeeze(scores),
+                    category_index,
+                    use_normalized_coordinates=True,
+                    line_thickness=4)
+                
+                total_passed_vehicle = total_passed_vehicle + counter           
 
-		# insert information text to video frame
-		font = cv2.FONT_HERSHEY_SIMPLEX
-		cv2.putText(input_frame,"Detected Vehicles: " + str(total_passed_vehicle), (10, 35), font, 0.8, (0,255,255),2,cv2.FONT_HERSHEY_SIMPLEX)
-	
-		# when the vehicle passed over line and counted, make the color of ROI line green
-		if(counter == 1):
-			cv2.line(input_frame,(0,200),(640,200),(0,255,0),5)
-		else:
-			cv2.line(input_frame,(0,200),(640,200),(0,0,255),5)
+                # insert information text to video frame
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(input_frame,"Detected Vehicles: " + str(total_passed_vehicle), (10, 35), font, 0.8, (0,255,255),2,cv2.FONT_HERSHEY_SIMPLEX)
+        
+                # when the vehicle passed over line and counted, make the color of ROI line green
+                if(counter == 1):
+                        cv2.line(input_frame,(0,200),(640,200),(0,255,0),5)
+                else:
+                        cv2.line(input_frame,(0,200),(640,200),(0,0,255),5)
 
-		# insert information text to video frame
-		cv2.rectangle(input_frame, (10, 275), (230, 337), (180, 132, 109), -1)
-		cv2.putText(input_frame,"ROI Line", (545, 190), font, 0.6,(0,0,255),2,cv2.LINE_AA)
-		cv2.putText(input_frame,"LAST PASSED VEHICLE INFO", (11, 290), font, 0.5, (255,255, 255), 1,cv2.FONT_HERSHEY_SIMPLEX)
-		cv2.putText(input_frame,"-Movement Direction: " + direction, (14, 302), font, 0.4, (255,255, 255), 1,cv2.FONT_HERSHEY_COMPLEX_SMALL)
-		cv2.putText(input_frame,"-Speed(km/h): " + speed, (14, 312), font, 0.4, (255, 255, 255), 1,cv2.FONT_HERSHEY_COMPLEX_SMALL)
-		cv2.putText(input_frame,"-Color: " + color, (14, 322), font, 0.4, (255,255, 255), 1,cv2.FONT_HERSHEY_COMPLEX_SMALL)
-		cv2.putText(input_frame,"-Vehicle Size/Type: " + size, (14, 332), font, 0.4, (255, 255, 255), 1,cv2.FONT_HERSHEY_COMPLEX_SMALL)
-	
-	    	cv2.imshow('vehicle detection',input_frame)
+                # insert information text to video frame
+                cv2.rectangle(input_frame, (10, 275), (230, 337), (180, 132, 109), -1)
+                cv2.putText(input_frame,"ROI Line", (545, 190), font, 0.6,(0,0,255),2,cv2.LINE_AA)
+                cv2.putText(input_frame,"LAST PASSED VEHICLE INFO", (11, 290), font, 0.5, (255,255, 255), 1,cv2.FONT_HERSHEY_SIMPLEX)
+                cv2.putText(input_frame,"-Movement Direction: " + direction, (14, 302), font, 0.4, (255,255, 255), 1,cv2.FONT_HERSHEY_COMPLEX_SMALL)
+                cv2.putText(input_frame,"-Speed(km/h): " + speed, (14, 312), font, 0.4, (255, 255, 255), 1,cv2.FONT_HERSHEY_COMPLEX_SMALL)
+                cv2.putText(input_frame,"-Color: " + color, (14, 322), font, 0.4, (255,255, 255), 1,cv2.FONT_HERSHEY_COMPLEX_SMALL)
+                cv2.putText(input_frame,"-Vehicle Size/Type: " + size, (14, 332), font, 0.4, (255, 255, 255), 1,cv2.FONT_HERSHEY_COMPLEX_SMALL)
+        
+                cv2.imshow('vehicle detection',input_frame)
 
-	    	if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
 
-		if(csv_line != "not_available"):
-			with open('traffic_measurement.csv', 'a') as f:
-				writer = csv.writer(f)				
-				size, color, direction, speed = csv_line.split(',')						
-				writer.writerows([csv_line.split(',')])		
-	    cap.release()
-	    cv2.destroyAllWindows()
-	    
+                if(csv_line != "not_available"):
+                        with open('traffic_measurement.csv', 'a') as f:
+                                writer = csv.writer(f)                          
+                                size, color, direction, speed = csv_line.split(',')                                             
+                                writer.writerows([csv_line.split(',')])         
+            cap.release()
+            cv2.destroyAllWindows()
+            
 object_detection_function()
+
