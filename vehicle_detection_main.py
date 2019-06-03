25c4407e52ba4aa1fe2e86c4523d73757b657ec5
ahmetozlu <ahmetozlu93@gmail.com>
2019-06-03 22:17:04 +0300

    requirements.txt added.

--- vehicle_detection_main.py
+++ vehicle_detection_main.py
@@ -40,7 +40,7 @@ if tf.__version__ < '1.4.0':
                       )
 
 # input video
-cap = cv2.VideoCapture('sub-1504614469486.mp4')
+cap = cv2.VideoCapture('sub-1504619634606.mp4')
 
 # Variables
 total_passed_vehicle = 0  # using it to count vehicles
